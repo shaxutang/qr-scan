12d5d7a0d04f282564f62fcf95ce35bdce1427ab
shaxutang <shaxutang@163.com>
2024-12-18 18:29:51 +0800

    remove: rm rule float button

--- src/components/FloatButtons.tsx
+++ src/components/FloatButtons.tsx
@@ -3,10 +3,8 @@ import {
   FullscreenExitOutlined,
   FullscreenOutlined,
   MoreOutlined,
-  SecurityScanOutlined,
 } from '@ant-design/icons'
 import { FloatButton } from 'antd'
-import { Link } from 'react-router-dom'
 
 const FloatButtons: React.FC = () => {
   const { isFullscreen, toggleFullscreenMode } = useFullscreen()
@@ -18,9 +16,6 @@ const FloatButtons: React.FC = () => {
       style={{ insetInlineEnd: 24 }}
       icon={<MoreOutlined />}
     >
-      <Link to="/rules">
-        <FloatButton icon={<SecurityScanOutlined />} />
-      </Link>
       <FloatButton
         icon={
           isFullscreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />
